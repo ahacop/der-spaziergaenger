b5328ac0117631f85a79f14ba5d640fd4cc74e3f
Ara Hacopian <arahacopian@gmail.com>
2018-02-02 13:38:35 +0100

    Fix indentation

--- src/lib_screen.asm
+++ src/lib_screen.asm
@@ -1,14 +1,14 @@
 .macro lib_screen_set_colors(border, bg0, bg1, bg2, bg3) {
-    lda #border
-    sta border_color
-    lda #bg0
-    sta background_color0
-    lda #bg1
-    sta background_color1
-    lda #bg2
-    sta background_color2
-    lda #bg3
-    sta background_color3
+  lda #border
+  sta border_color
+  lda #bg0
+  sta background_color0
+  lda #bg1
+  sta background_color1
+  lda #bg2
+  sta background_color2
+  lda #bg3
+  sta background_color3
 }
 
 .macro lib_screen_clear_screen(screen, clearByte) {
