4da04dbddfbb9bbece75ffb42d0b3d70fe3b5e76
Ara Hacopian <arahacopian@gmail.com>
2018-02-02 13:19:23 +0100

    Replace screen/color ram tables with .fill

--- src/config_symbols.asm
+++ src/config_symbols.asm
@@ -39,47 +39,12 @@
 .label ZeroPageHigh2   = $FE
 // $FF       Reserved for Kernal
 
-
 ScreenRAMRowStartLow: // screen_ram + 40*0, 40*1, 40*2 ... 40*24
-  .byte <screen_ram,     <screen_ram+40,  <screen_ram+80
-  .byte <screen_ram+120, <screen_ram+160, <screen_ram+200
-  .byte <screen_ram+240, <screen_ram+280, <screen_ram+320
-  .byte <screen_ram+360, <screen_ram+400, <screen_ram+440
-  .byte <screen_ram+480, <screen_ram+520, <screen_ram+560
-  .byte <screen_ram+600, <screen_ram+640, <screen_ram+680
-  .byte <screen_ram+720, <screen_ram+760, <screen_ram+800
-  .byte <screen_ram+840, <screen_ram+880, <screen_ram+920
-  .byte <screen_ram+960
-
+  .fill 25, <screen_ram + i*40
 ScreenRAMRowStartHigh: // screen_ram + 40*0, 40*1, 40*2 ... 40*24
-  .byte >screen_ram,     >screen_ram+40,  >screen_ram+80
-  .byte >screen_ram+120, >screen_ram+160, >screen_ram+200
-  .byte >screen_ram+240, >screen_ram+280, >screen_ram+320
-  .byte >screen_ram+360, >screen_ram+400, >screen_ram+440
-  .byte >screen_ram+480, >screen_ram+520, >screen_ram+560
-  .byte >screen_ram+600, >screen_ram+640, >screen_ram+680
-  .byte >screen_ram+720, >screen_ram+760, >screen_ram+800
-  .byte >screen_ram+840, >screen_ram+880, >screen_ram+920
-  .byte >screen_ram+960
+  .fill 25, >screen_ram + i*40
 
 ColorRAMRowStartLow: // color_ram + 40*0, 40*1, 40*2 ... 40*24
-  .byte <color_ram,     <color_ram+40,  <color_ram+80
-  .byte <color_ram+120, <color_ram+160, <color_ram+200
-  .byte <color_ram+240, <color_ram+280, <color_ram+320
-  .byte <color_ram+360, <color_ram+400, <color_ram+440
-  .byte <color_ram+480, <color_ram+520, <color_ram+560
-  .byte <color_ram+600, <color_ram+640, <color_ram+680
-  .byte <color_ram+720, <color_ram+760, <color_ram+800
-  .byte <color_ram+840, <color_ram+880, <color_ram+920
-  .byte <color_ram+960
-
+  .fill 25, <color_ram + i*40
 ColorRAMRowStartHigh: // color_ram + 40*0, 40*1, 40*2 ... 40*24
-  .byte >color_ram,     >color_ram+40,  >color_ram+80
-  .byte >color_ram+120, >color_ram+160, >color_ram+200
-  .byte >color_ram+240, >color_ram+280, >color_ram+320
-  .byte >color_ram+360, >color_ram+400, >color_ram+440
-  .byte >color_ram+480, >color_ram+520, >color_ram+560
-  .byte >color_ram+600, >color_ram+640, >color_ram+680
-  .byte >color_ram+720, >color_ram+760, >color_ram+800
-  .byte >color_ram+840, >color_ram+880, >color_ram+920
-  .byte >color_ram+960
+  .fill 25, >color_ram + i*40
